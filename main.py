795156ee12ce43e3f6ab046d62b995072b4e372c
jholland-vin-dit <jholland-vin-dit@users.noreply.github.com>
2023-10-10 13:56:29 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,112 @@
+def on_on_overlap(sprite2, otherSprite2):
+    otherSprite2.set_position(randint(10, 142), randint(10, 102))
+    otherSprite2.set_velocity(randint(10, 20), randint(10, 20))
+    info.change_life_by(-1)
+    music.play(music.melody_playable(music.power_down),
+        music.PlaybackMode.UNTIL_DONE)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap)
+
+def on_life_zero():
+    game.game_over(False)
+info.on_life_zero(on_life_zero)
+
+def on_on_overlap2(sprite, otherSprite):
+    if list2.index(otherSprite.image) == 0:
+        info.change_score_by(1)
+    else:
+        info.change_score_by(2)
+    music.play(music.melody_playable(music.power_up),
+        music.PlaybackMode.UNTIL_DONE)
+    otherSprite.start_effect(effects.cool_radial)
+    sprites.destroy(otherSprite)
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap2)
+
+myfood: Sprite = None
+list2: List[Image] = []
+info.set_life(3)
+list2 = [img("""
+        . . . . . . b b b b . . . . . . 
+            . . . . . . b 4 4 4 b . . . . . 
+            . . . . . . b b 4 4 4 b . . . . 
+            . . . . . b 4 b b b 4 4 b . . . 
+            . . . . b d 5 5 5 4 b 4 4 b . . 
+            . . . . b 3 2 3 5 5 4 e 4 4 b . 
+            . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+            . . . b 5 3 2 3 5 5 5 5 e e e e 
+            . . b d 7 5 5 5 3 2 3 5 5 e e e 
+            . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+            . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+            . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+            b d 3 2 d 5 5 5 d d d 4 4 . . . 
+            b 5 5 5 5 d d 4 4 4 4 . . . . . 
+            4 d d d 4 4 4 . . . . . . . . . 
+            4 4 4 4 . . . . . . . . . . . .
+    """),
+    img("""
+        . . . . c c c b b b b b . . . . 
+            . . c c b 4 4 4 4 4 4 b b b . . 
+            . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+            . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+            e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+            e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+            e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+            . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+            8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+            8 7 2 e e e e e e e e e e 2 7 8 
+            e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+            e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+            e b e 8 8 c c 8 8 c c c 8 e b e 
+            e e b e c c e e e e e c e b e e 
+            . e e b b 4 4 4 4 4 4 4 4 e e . 
+            . . . c c c c c e e e e e . . .
+    """)]
+Joe = sprites.create(img("""
+        . . . . f f f f f . . . . . . . 
+            . . . f e e e e e f . . . . . . 
+            . . f d d d d e e e f . . . . . 
+            . c d f d d f d e e f f . . . . 
+            . c d f d d f d e e d d f . . . 
+            c d e e d d d d e e b d c . . . 
+            c d d d d c d d e e b d c . f f 
+            c c c c c d d d e e f c . f e f 
+            . f d d d d d e e f f . . f e f 
+            . . f f f f f e e e e f . f e f 
+            . . . . f e e e e e e e f f e f 
+            . . . f e f f e f e e e e f f . 
+            . . . f e f f e f e e e e f . . 
+            . . . f d b f d b f f e f . . . 
+            . . . f d d c d d b b d f . . . 
+            . . . . f f f f f f f f f . . .
+    """),
+    SpriteKind.player)
+Joe.set_position(80, 60)
+Joe.set_stay_in_screen(True)
+controller.move_sprite(Joe)
+my_enemy = sprites.create(img("""
+        . . . . . c c c c c c c . . . . 
+            . . . . c 6 7 7 7 7 7 6 c . . . 
+            . . . c 7 c 6 6 6 6 c 7 6 c . . 
+            . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+            . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+            . . f 7 8 1 f f 1 6 7 7 7 f . . 
+            . . f 6 f 1 f f 1 f 7 7 7 f . . 
+            . . . f f 2 2 2 2 f 7 7 6 f . . 
+            . . c c f 2 2 2 2 7 7 6 f c . . 
+            . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+            c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+            f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+            f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+            f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+            . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+            . . f f c c c c c c c c . . . .
+    """),
+    SpriteKind.enemy)
+my_enemy.set_bounce_on_wall(True)
+my_enemy.set_position(randint(10, 142), randint(10, 102))
+my_enemy.set_velocity(randint(10, 20), randint(10, 20))
+
+def on_update_interval():
+    global myfood
+    myfood = sprites.create(list2[randint(0, 1)], SpriteKind.food)
+    myfood.set_position(randint(10, 142), randint(10, 102))
+game.on_update_interval(3000, on_update_interval)
---
+++ main.ts
@@ -0,0 +1,103 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite2, otherSprite2) {
+    otherSprite2.setPosition(randint(10, 142), randint(10, 102))
+    otherSprite2.setVelocity(randint(10, 20), randint(10, 20))
+    info.changeLifeBy(-1)
+    music.play(music.melodyPlayable(music.powerDown), music.PlaybackMode.UntilDone)
+})
+info.onLifeZero(function () {
+    game.gameOver(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    if (list.indexOf(otherSprite.image) == 0) {
+        info.changeScoreBy(1)
+    } else {
+        info.changeScoreBy(2)
+    }
+    otherSprite.startEffect(effects.coolRadial)
+    sprites.destroy(otherSprite)
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.UntilDone)
+})
+let myfood: Sprite = null
+let list: Image[] = []
+info.setLife(3)
+list = [img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, img`
+    . . . . c c c b b b b b . . . . 
+    . . c c b 4 4 4 4 4 4 b b b . . 
+    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+    8 7 2 e e e e e e e e e e 2 7 8 
+    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+    e b e 8 8 c c 8 8 c c c 8 e b e 
+    e e b e c c e e e e e c e b e e 
+    . e e b b 4 4 4 4 4 4 4 4 e e . 
+    . . . c c c c c e e e e e . . . 
+    `]
+let Joe = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.Player)
+Joe.setPosition(80, 60)
+Joe.setStayInScreen(true)
+controller.moveSprite(Joe)
+let my_enemy = sprites.create(img`
+    . . . . . c c c c c c c . . . . 
+    . . . . c 6 7 7 7 7 7 6 c . . . 
+    . . . c 7 c 6 6 6 6 c 7 6 c . . 
+    . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+    . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+    . . f 7 8 1 f f 1 6 7 7 7 f . . 
+    . . f 6 f 1 f f 1 f 7 7 7 f . . 
+    . . . f f 2 2 2 2 f 7 7 6 f . . 
+    . . c c f 2 2 2 2 7 7 6 f c . . 
+    . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+    c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+    f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+    f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+    . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+    . . f f c c c c c c c c . . . . 
+    `, SpriteKind.Enemy)
+my_enemy.setBounceOnWall(true)
+my_enemy.setPosition(randint(10, 142), randint(10, 102))
+my_enemy.setVelocity(randint(10, 20), randint(10, 20))
+game.onUpdateInterval(3000, function () {
+    myfood = sprites.create(list[randint(0, 1)], SpriteKind.Food)
+    myfood.setPosition(randint(10, 142), randint(10, 102))
+})
